922d778eb712944d0aad36060b01329f50e9d923
ttmocha <tythie1972@gmail.com>
2025-10-17 10:40:58 +1000

    Fixed navigation structure, duplicate folder error

--- IAB207-main/website/forms.py
+++ IAB207-main/website/forms.py
@@ -19,3 +19,19 @@ class RegisterForm(FlaskForm):
 
     # submit button
     submit = SubmitField("Register")
+
+class EventForm(FlaskForm):
+    title = StringField("Event Name", validators=[InputRequired(), Length(max=120)])
+    image_url = StringField("Event Image (URL)", validators=[Length(max=300)])
+    date = StringField("Date (YYYY-MM-DD)", validators=[InputRequired(), Length(max=20)])
+    start_time = StringField("Start Time (HH:MM)", validators=[InputRequired(), Length(max=10)])
+    end_time = StringField("End Time (HH:MM)", validators=[InputRequired(), Length(max=10)])
+    server_region = StringField("Server Region", validators=[InputRequired(), Length(max=50)])
+    team_size = StringField("Team Size (Solo/Duos/Trios/Squads)", validators=[InputRequired(), Length(max=20)])
+    game_mode = StringField("Game Mode", validators=[Length(max=60)])
+    buy_in = StringField("Buy In", validators=[Length(max=20)])
+    prize = StringField("Prize Pool", validators=[Length(max=60)])
+    match_code = StringField("Match Code / Epic ID", validators=[Length(max=60)])
+    description = TextAreaField("Description / Rules", validators=[InputRequired(), Length(max=2000)])
+    submit = SubmitField("Create Event")
+    
