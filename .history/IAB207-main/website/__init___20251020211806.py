37dc89bf24a60a3f7c26787e55d341a40248f2d7
Sosaiah Hillier-Barnes <162547194+Hillier-Barnes@users.noreply.github.com>
2025-10-20 22:30:58 +1000

    feat: create login and register page

---
+++ .history/IAB207-main/website/__init___20251020211806.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.debug = True
+    app.secret_key = 'somesecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
+    db.init_app(app)
+
+    Bootstrap5(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.scalar(db.select(User).where(User.id==user_id))
+
+    from .views import main_bp
+    app.register_blueprint(main_bp)
+
+    try:
+        from .auth import auth_bp
+        app.register_blueprint(auth_bp)
+    except Exception:
+        pass
+
+    return app
